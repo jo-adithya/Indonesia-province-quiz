e024a1d2f8651f21ab8c7c44f370a9e35a6a6d46
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-12 15:15:25 +0700

    start the game

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.setup(width=1240, height=616)
 screen.title('Indonesian Provinces Quiz')
-image_path = 'Indonesian Map (easy).gif'
-screen.addshape(image_path)
 
-turtle.shape(image_path)
+# Choose level of difficulty
+while True:
+    difficulty = screen.textinput(title='Difficulty Level',
+                                  prompt='Choose the level of difficulty, easy or hard?').lower()
+    if difficulty not in ['easy', 'hard']:
+        continue
+    image_path = 'Indonesian Map (' + difficulty + ').gif'
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+    break
 
+tim = turtle.Turtle()
+tim.penup()
+tim.hideturtle()
+tim.speed('fastest')
+df = pd.read_csv('Indonesian_province.csv')
+provinces = [' '.join(x.lower().split()) for x in df['province']]
+score = 0
 
-def get_mouse_click(x, y):
-    print(f'({x}, {y})\n')
+while score < 34:
+    guess = screen.textinput(title=f'{score}/34 Provinces Correct', prompt='Province name in Indonesia:').lower()
+    if guess in provinces:
+        prov_data = df.iloc[provinces.index(guess)]
+        tim.goto(prov_data['x'], prov_data['y'])
+        tim.color(prov_data['color'])
+        tim.write(prov_data['province'], align='right', font=('Arial', 14, 'bold'))
+        score += 1
 
-
-# Get the mouseclick coordinates relative to the image
-turtle.onscreenclick(get_mouse_click)
 turtle.mainloop()
-
